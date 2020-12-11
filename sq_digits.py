9490b1f925e1a5c8708d82a1f1ef69645bd55dbc
IgorPronin <yohuh@inbox.ru>
2020-12-11 16:46:11 +0200

    str --> int

--- sq_digits.py
+++ sq_digits.py
@@ -1,11 +1,12 @@
 """
 Square every digit of a number. For example, if we run 9119 through the function,
 811181 will come out, because 92 is 81 and 12 is 1.
+Note: The function accepts an integer and returns an integer
 """
 
 
 def square_digits(num):
-    return ''.join(str(int(x) ** 2) for x in str(num))
+    return str(''.join(str(int(x) ** 2) for x in str(num)))
 
 
 if __name__ == '__main__':
