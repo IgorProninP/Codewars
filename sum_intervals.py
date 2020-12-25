64321497804d50d2ac78a8fe8a1e3742c14c5f09
IgorPronin <yohuh@inbox.ru>
2020-12-25 20:53:49 +0200

    Function that accepts an array of intervals, and returns the sum of all the interval lengths.

---
+++ sum_intervals.py
@@ -0,0 +1,76 @@
+"""
+Write a function called sumIntervals/sum_intervals() that accepts an array of intervals, and returns
+the sum of all the interval lengths. Overlapping intervals should only be counted once.
+
+Intervals
+Intervals are represented by a pair of integers in the form of an array. The first value of the interval
+will always be less than the second value. Interval example: [1, 5] is an interval from 1 to 5. The length
+of this interval is 4.
+
+Overlapping Intervals
+List containing overlapping intervals:
+
+[
+   [1,4],
+   [7, 10],
+   [3, 5]
+]
+The sum of the lengths of these intervals is 7. Since [1, 4] and [3, 5] overlap, we can treat the interval
+as [1, 5], which has a length of 4.
+
+Examples:
+sumIntervals( [
+   [1,2],
+   [6, 10],
+   [11, 15]
+] ); // => 9
+
+sumIntervals( [
+   [1,4],
+   [7, 10],
+   [3, 5]
+] ); // => 7
+
+sumIntervals( [
+   [1,5],
+   [10, 20],
+   [1, 6],
+   [16, 19],
+   [5, 11]
+] ); // => 19
+"""
+
+
+# Solutions from codewars:
+#
+# def sum_of_intervals(intervals):
+#     return len(set([n for (a, b) in intervals for n in [i for i in range(a, b)]]))
+#
+# def sum_of_intervals(intervals):
+#     s = []
+#     for i in intervals:
+#         s += list(range(i[0],i[1]))
+#     return len(set(s))
+#
+# from itertools import chain
+#
+# def sum_of_intervals(intervals):
+#     return len(set(chain.from_iterable(range(*i) for i in intervals)))
+
+def sum_of_intervals(intervals):
+    total = set()
+    for begin, end in intervals:
+        for num in range(begin, end):
+            total.add(num)
+
+    return len(total)
+
+
+if __name__ == '__main__':
+    print(sum_of_intervals([
+        [1, 5],
+        [10, 20],
+        [1, 6],
+        [16, 19],
+        [5, 11]
+    ]))
