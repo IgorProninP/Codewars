3baddb8a6f1ee593b3790782ee1715ab5974c11f
IgorPronin <yohuh@inbox.ru>
2021-01-06 20:38:38 +0200

    Takes a list of integers in increasing order and returns a correctly formatted string in the range format.

---
+++ range_extraction.py
@@ -0,0 +1,41 @@
+"""
+A format for expressing an ordered list of integers is to use a comma separated list of either
+individual integers or a range of integers denoted by the starting integer separated from the end
+integer in the range by a dash, '-'. The range includes all integers in the interval including both
+endpoints.
+It is not considered a range unless it spans at least 3 numbers. For example "12,13,15-17"
+Complete the solution so that it takes a list of integers in increasing order and returns a correctly
+formatted string in the range format.
+
+Example:
+
+solution([-6, -3, -2, -1, 0, 1, 3, 4, 5, 7, 8, 9, 10, 11, 14, 15, 17, 18, 19, 20])
+# returns "-6,-3-1,3-5,7-11,14,15,17-20"
+"""
+
+
+def solution(args):
+    result = []
+    temp = [args[0]]
+    for digit in args[1:]:
+        if digit - temp[-1] > 1:
+            if len(temp) > 2:
+                result.append(f'{temp[0]}-{temp[-1]}')
+                temp = [digit]
+            else:
+                for num in temp:
+                    result.append(str(num))
+                temp = [digit]
+        else:
+            temp.append(digit)
+    if len(temp) > 2:
+        result.append(f'{temp[0]}-{temp[-1]}')
+    else:
+        for i in temp:
+            result.append(str(i))
+    return ','.join(result)
+
+
+if __name__ == '__main__':
+    data = [-6, -3, -2, -1, 0, 1, 3, 4, 5, 7, 8, 9, 10, 11, 14, 15, 17, 18, 19, 20, 22, 23]
+    print(solution(data))
